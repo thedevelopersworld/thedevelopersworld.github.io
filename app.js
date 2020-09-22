83a33135b49a5485bdbbca82dd9e90228cfd0aac
Kaustubh Bhule <69647483+thedevelopersworld@users.noreply.github.com>
2020-09-22 19:22:25 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+function menu_open(){
+
+    var open =  document.getElementById("navicon1");
+    var close =  document.getElementById("navicon2");
+    var menu = document.getElementById("menu");
+    var searchbox = document.getElementById('search');
+    open.style["visibility"]="hidden";
+    close.style["display"]="inline-block";
+    menu.style["visibility"]="visible";
+    searchbox.style['margin-top']="40px";
+    setTimeout(function(){   
+        menu_close(); 
+     }, 3000)
+ }
+
+ function menu_close(){
+    var open =  document.getElementById("navicon1");
+    var close =  document.getElementById("navicon2");
+    var menu = document.getElementById("menu");
+    var searchbox = document.getElementById('search');
+    close.style["display"]="none";
+    open.style["visibility"]="visible";
+    menu.style["visibility"]="hidden";
+    searchbox.style['margin-top']="-100px";
+ }
